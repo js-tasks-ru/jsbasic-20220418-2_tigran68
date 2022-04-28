6e1ce8edd48c43c706f8f89616f3ea59a24f6055
tigran <tigran@yandex.ru>
2022-04-28 22:16:11 +0300

    Выполнил 4 модуль

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length;i++) {
+    let checkStatus = table.rows[i].cells[3];
+    switch (checkStatus.dataset.available) {
+    case "true":
+      table.rows[i].classList.add("available");
+      break;
+    case "false":
+      table.rows[i].classList.add("unavailable");
+      break;
+    default:
+      table.rows[i].setAttribute('hidden', 'true');
+      table.rows[0].removeAttribute('hidden');
+      break;
+    }
+    let checkGender = table.rows[i].cells[2];
+    switch (checkGender.innerHTML) {
+    case "m":
+      table.rows[i].classList.add('male');
+      break;
+    case "f":
+      table.rows[i].classList.add('female');
+      break;
+    }
+    let checkAge = table.rows[i].cells[1];
+    if (parseInt(checkAge.innerHTML) < 18) {
+      table.rows[i].style = "text-decoration: line-through";
+    }
+  }
 }
